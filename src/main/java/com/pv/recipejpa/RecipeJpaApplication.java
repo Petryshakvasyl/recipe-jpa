39447a63da37cc592c956cc8184dfbbc29413798
VasylPetryshak <vasylpetryshak@gmial.com>
2018-10-09 22:56:38 +0200

    start project

---
+++ src/main/java/com/pv/recipejpa/RecipeJpaApplication.java
@@ -0,0 +1,12 @@
+package com.pv.recipejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeJpaApplication.class, args);
+	}
+}
